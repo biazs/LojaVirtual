0bc2925a0af149ba29c233355ac03c5e93d6df2d
Bia Zoroastro <fabianazs@gmail.com>
2019-12-17 15:57:52 -0300

    Classe categoria

---
+++ LojaVirtual/Models/Categoria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        /*
+         * www.lojavirtual.com.br/categoria/5
+         * www.lojavirtual.com.br/categoria/informatica (url amigável)
+         * 
+         * Exemplo de Slug: 
+         *  Nome: Telefone sem fio
+         *  Slug: telefone-sem-fio         
+         */
+
+        public string Slug { get; set; }
+
+        /*
+         * Auto-relacionamento
+         * 1-Informatica - Pai:null
+         *  2- Mouse - Pai:1
+         *      3- Mouse sem fio - Pai:2
+         *      4- Mouse Gamer - Pai:3
+         */
+
+        public int? CategoriaPaiId { get; set; }
+
+        [ForeignKey("CategoriaPaiId")]
+        public virtual Categoria CategoriaPai { get; set; }
+
+
+    }
+}
