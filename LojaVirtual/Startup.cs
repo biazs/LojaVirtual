3b8c8ca9400ee69e9e59ce8c765103c0cb95c057
Bia Zoroastro <fabianazs@gmail.com>
2019-12-17 09:47:16 -0300

    Criar Area Colaborador

--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -82,6 +82,10 @@ namespace LojaVirtual
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                      name: "areas",
+                      template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
