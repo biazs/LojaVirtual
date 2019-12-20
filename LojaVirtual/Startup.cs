e532f34369e91fd6b0c7d5cd5a1db52d04971b0b
Bia Zoroastro <fabianazs@gmail.com>
2019-12-20 10:50:45 -0300

    Repository Categoria

--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -35,6 +35,7 @@ namespace LojaVirtual
             services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddScoped<INewsletterRepository, NewsletterRepository>();
             services.AddScoped<IColaboradorRepository, ColaboradorRepository>();
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 
             services.Configure<CookiePolicyOptions>(options =>
             {
