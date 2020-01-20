5ded4af119d82f40ed76ae0fcdb4d3d6767d3bad
Bia Zoroastro <fabianazs@gmail.com>
2020-01-20 16:45:01 -0300

    Modelo Produto

--- LojaVirtual/Models/Produto.cs
+++ LojaVirtual/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,24 @@ namespace LojaVirtual.Models
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        //frete - correios
+        public double Peso { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Comprimento { get; set; }
+
+        //Banco de dados - Relacionamento entre tabelas
+        public int CategoriaId { get; set; }
+
+        //POO - Associação entre objetos
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        /*
+         * EF - ORM - Biblioteca Unir - Banco de dados e POO (ORM - Mapeamento de Objetos <-> Relacionamento)
+         * Fluente API - Attributes
+        */
     }
 }
