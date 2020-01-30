0575b3d710edc813d2af81f7dd435a7ba31cc800
Bia Zoroastro <fabianazs@gmail.com>
2020-01-30 16:27:54 -0300

    Upload - Controlador Imagem

---
+++ LojaVirtual/Areas/Colaborador/Controllers/ImagemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    public class ImagemController : Controller
+    {
+        public IActionResult Armazenar(IFormFile file)
+        {            
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+    }
+}
