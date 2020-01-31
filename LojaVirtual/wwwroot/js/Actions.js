e4dce5ae9fad18998f9dc4ebafb34232e41f7f01
Bia Zoroastro <fabianazs@gmail.com>
2020-01-31 15:57:50 -0300

    Upload - Javascript - Part1

--- LojaVirtual/wwwroot/js/Actions.js
+++ LojaVirtual/wwwroot/js/Actions.js
@@ -1,5 +1,5 @@
-﻿$(document).ready(function () {
-    alert("teste");
+﻿$(document).ready(function () {    
+    console.log("teste");
     $(".btn-danger").click(function (e) {
         var resultado = confirm("Tem certeza que deseja realizar esta operação?");
 
@@ -8,4 +8,13 @@
         }
     });
     $('.dinheiro').mask('000.000.000.000.000,00', { reverse: true });
+
+    AjaxUploadImagemProduto();
+
 });
+
+function AjaxUploadImagemProduto() {
+    $(".img-upload").click(function() {
+        $(this).parent().find(".input-file").click();
+    })
+}
