42377719de5831316c2905ca2df2b50d6254f8eb
Bia Zoroastro <fabianazs@gmail.com>
2020-02-07 16:43:33 -0300

    JavaScript FormData

--- LojaVirtual/wwwroot/js/Actions.js
+++ LojaVirtual/wwwroot/js/Actions.js
@@ -15,4 +15,13 @@ function AjaxUploadImagemProduto() {
     $(".img-upload").click(function () {
         $(this).parent().find(".input-file").click();
     });
+
+    $(".input-file").change(function (){
+        //Formulario de dados via JS
+        var Binario = $(this)[0].files[0];
+        var Formulario = new FormData();
+        Formulario.append("file", Binario);
+
+        //TODO: Requisicao Ajax enviando Formulario
+    });
 }
