d1e064e218611c3765a827674480d375ba4cc1f3
Bia Zoroastro <fabianazs@gmail.com>
2020-03-11 17:14:39 -0300

    Menu - ViewComponent

---
+++ LojaVirtual/Libraries/Component/MenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Libraries.Component
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        //TODO CategoriaRepository
+
+        ICategoriaRepository _categoriaRepository;
+        public MenuViewComponent(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        //TODO logica do componente
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var ListaCategoria = _categoriaRepository.ObterTodasCategorias().ToList();
+            return View();
+        }
+    }
+}
